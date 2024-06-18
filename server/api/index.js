1ea98a0721f20e64887b95c79a51e28638782189
tehbotolbayu <bayuuabdur2903@gmail.com>
2024-06-18 19:04:04 +0800

    fix import

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const routers = require('../routers');
 const cors = require('cors');
-const {main} = require("./models")
+const {main} = require("../models")
 
 main();
 
